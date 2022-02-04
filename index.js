da4b1527ed37e0cd19967637e4d67559c358c64b
if3lc <e-ben@msn.com>
2022-02-05 00:04:12 +0300

    Create simple API

--- index.js
+++ index.js
@@ -32,6 +32,60 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
+//Get all posts
+app.get('/api/posts',(req, res) => {
+    let sql = 'SELECT * FROM posts'
+    db.query(sql, (err, row, fields) => {
+        if (err) throw err
+        res.json(row)
+    })
+});
+
+//Get single post
+app.get('/api/posts/:id', (req, res) => {
+    let sql = `SELECT * FROM posts WHERE id = ${req.params.id}`
+    db.query(sql, (err, row, fields) => {
+        if (err) throw err
+        res.json(row)
+    })
+})
+
+//Create post
+app.post('/api/posts', (req, res) => {
+    let sql = `INSERT INTO posts (title, body) VALUES ('${req.body.title}', '${req.body.body}')`
+    db.query(sql, (err, row, fields) => {
+        if (err) throw err
+        res.json(row)
+    })
+})
+
+//Get all categories
+app.get('/api/categories', (req, res) => {
+    let sql = 'SELECT * FROM categories'
+    db.query(sql, (err, row, fields) => {
+        if (err) throw err
+        res.json(row)
+    })
+})
+
+//Get single category
+app.get('/api/categories/:id', (req, res) => {
+    let sql = `SELECT * FROM categories WHERE id = ${req.params.id}`
+    db.query(sql, (err, row, fields) => {
+        if (err) throw err
+        res.json(row)
+    })
+})
+
+//Create category
+app.post('/api/categories', (req, res) => {
+    let sql = `INSERT INTO categories (title, content) VALUES ('${req.body.title}', '${req.body.content}')`
+    db.query(sql, (err, row, fields) => {
+        if (err) throw err
+        res.json(row)
+    })
+})
+
 //Server start
 app.listen(process.env.PORT, () => {
     console.log('Server is runing on http://localhost:' + process.env.PORT)
